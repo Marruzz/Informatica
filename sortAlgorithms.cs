945fcfe0c8388b743b0c3560d452cc108d27ade3
Marruzz <luca.marroni.07@outlook.com>
2024-04-08 13:09:39 +0200

    Update sortAlgorithms.cs

--- sortAlgorithms.cs
+++ sortAlgorithms.cs
@@ -56,15 +56,20 @@ foreach (int i in arraySelection)
 //metodi
 static void bubbleSort(int[] arrayBubble)
 {
+    //temporanea variabile
     int temp;
     for (int i = 0; i < arrayBubble.Length; i++)
     {
         for (int j = 0; j < arrayBubble.Length - 1; j++)
         {
+            //se arrayBubble[j] è maggiore di arrayBubble[j + 1]
             if (arrayBubble[j] > arrayBubble[j + 1])
             {
+                //temp ottiene il valore di arrayBubble[j + 1]
                 temp = arrayBubble[j + 1];
+                //arrayBubble[j + 1] ottiene il valore di arrayBubble[j]
                 arrayBubble[j + 1] = arrayBubble[j];
+                //arrayBubble[j] ottiene il valore di temp
                 arrayBubble[j] = temp;
             }
         }
@@ -73,35 +78,49 @@ static void bubbleSort(int[] arrayBubble)
 
 static void insertionSort(int[] arrayInsertion)
 {
+    //key è la variabile temporanea
     int key, y;
     for (int i = 1; i < arrayInsertion.Length; i++)
     {
+        //key ottiene il valore di arrayInsertion[i]
         key = arrayInsertion[i];
+        //y ottiene il valore di i - 1
         y = i - 1;
+        //finchè y è maggiore o uguale a 0 e arrayInsertion[y] è maggiore di key
         while (y >= 0 && arrayInsertion[y] > key)
         {
+            //arrayInsertion[y + 1] ottiene il valore di arrayInsertion[y]
             arrayInsertion[y + 1] = arrayInsertion[y];
+            //y ottiene il valore di y - 1
             y = y - 1;
         }
+        //arrayInsertion[y + 1] ottiene il valore di key
         arrayInsertion[y + 1] = key;
     }
 }
 
 static void selectionSort(int[] arraySelection)
 {
+    //minIndex è la variabile temporanea
     int minIndex, temp1;
     for (int i = 0; i < arraySelection.Length - 1; i++)
     {
+        //minIndex ottiene il valore di i
         minIndex = i;
         for (int j = i + 1; j < arraySelection.Length; j++)
         {
+            //se arraySelection[j] è minore di arraySelection[minIndex]
             if (arraySelection[j] < arraySelection[minIndex])
             {
+                //minIndex ottiene il valore di j
                 minIndex = j;
             }
         }
+        //temp1 ottiene il valore di arraySelection[i]
         temp1 = arraySelection[i];
+        //arraySelection[i] ottiene il valore di arraySelection[minIndex]
         arraySelection[i] = arraySelection[minIndex];
+        //arraySelection[minIndex] ottiene il valore di temp1
         arraySelection[minIndex] = temp1;
     }
 }
