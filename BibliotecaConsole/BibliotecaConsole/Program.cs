1443ad1e8882d76524f086aaad9b335e2b71a244
Marruzz <luca.marroni.07@outlook.com>
2024-09-30 12:29:08 +0200

    aggiunta del ciclo while
    messo per far continuare l'aggiunta di libri finché non basta

--- BibliotecaConsole/BibliotecaConsole/Program.cs
+++ BibliotecaConsole/BibliotecaConsole/Program.cs
@@ -1,13 +1,18 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿Console.WriteLine("Gestione Biblioteca");
 
-Console.WriteLine("Gestione Biblioteca");
-
-Console.WriteLine("Inserimento libri: ");
-Console.Write("Inserisci il titolo: ");
-string titolo = Console.ReadLine(); 
-Console.Write("Inserisci l'autore: ");
-string autore = Console.ReadLine(); 
-Console.Write("Inserisci il numero di pagine: ");
-int pagine =int.Parse(Console.ReadLine());
-Console.Write("Inserisci il prezzo: ");
-decimal prezzo = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Inserimento libri");
+bool continua = true;
+while (continua)
+{
+    Console.Write("Inserisci il titolo: ");
+    string titolo = Console.ReadLine();
+    Console.Write("Inserisci l'autore: ");
+    string autore = Console.ReadLine();
+    Console.Write("Inserisci il numero di pagine: ");
+    int pagine = int.Parse(Console.ReadLine());
+    Console.Write("Inserisci il prezzo: ");
+    decimal prezzo = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Si vuole inserire un altro libro? y/n");
+    string risposta = Console.ReadLine();
+    if (risposta == "n") continua = false;
+}
