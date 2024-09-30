b6d9157d7953a13831c92260617036e0ef6b2bc0
Marruzz <luca.marroni.07@outlook.com>
2024-09-30 12:23:31 +0200

    Creazione del file e aggiunta della struttura

---
+++ BibliotecaConsole/BibliotecaConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Gestione Biblioteca");
+
+Console.WriteLine("Inserimento libri: ");
+Console.Write("Inserisci il titolo: ");
+string titolo = Console.ReadLine(); 
+Console.Write("Inserisci l'autore: ");
+string autore = Console.ReadLine(); 
+Console.Write("Inserisci il numero di pagine: ");
+int pagine =int.Parse(Console.ReadLine());
+Console.Write("Inserisci il prezzo: ");
+decimal prezzo = decimal.Parse(Console.ReadLine());
