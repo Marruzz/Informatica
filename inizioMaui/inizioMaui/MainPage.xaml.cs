9b484b2ead4438342d84c19e171be723920aecf1
Marruzz <luca.marroni.07@outlook.com>
2024-10-03 11:45:53 +0200

    programma finito

--- inizioMaui/inizioMaui/MainPage.xaml.cs
+++ inizioMaui/inizioMaui/MainPage.xaml.cs
@@ -1,24 +1,53 @@
-﻿namespace inizioMaui
+﻿
+namespace inizioMaui
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        int indovinaNumero;
+        int counter;
 
         public MainPage()
         {
             InitializeComponent();
+            Random numero = new Random();
+            indovinaNumero = (int)numero.Next(1, 50);
+            counter = 0;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void btnNumber_Click(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                counter++;
+                int numeroIndovinato = int.Parse(entryNumber.Text);
+                if (numeroIndovinato == indovinaNumero)
+                {
+                    lblHint.Text = "Hai Vinto in " + counter + " tentativi!";
+                    lblTentativi.Text = ""; 
+                    return;
+                }
+                else if (numeroIndovinato > indovinaNumero)
+                {
+                    if (numeroIndovinato > 50) { await DisplayAlert("Errore", "Il numero deve essere minore di 50", "OK, ne reinserisco un altro"); counter--; }
+                    lblHint.Text = "Il numero da indovinare è più piccolo";
+                }
+                else
+                {
+                    lblHint.Text = "Il numero da indovinare è più grande";
+                }
+                lblTentativi.Text = "Hai fatto " + counter + " tentativi"; 
+            }
+            catch
+            {
+                await DisplayAlert("Errore", "Si inserisca un numero", "OK");
+            }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private async Task DisplayAlert(string v)
+        {
+            throw new NotImplementedException();
         }
     }
 
